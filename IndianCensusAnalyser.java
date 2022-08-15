6f5e87279a399541522f620daf4960e7bec1430c
RahulPatil-N7 <rahul.patil.3125@gmail.com>
2022-08-15 20:23:31 +0530

    Added : Welcome message.

---
+++ IndianCensusAnalyser.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.CensusAnalyser;
+
+public class IndianCensusAnalyser {
+
+	public static void main(String[] args) {
+
+		System.out.println("******************Welcome To Indian Census Analyzer *****************\n");
+
+	}
+
+}
